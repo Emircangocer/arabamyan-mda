ca1b38a83a6cc4efe644552b281d89095cbdca17
fatih gungor <gngrfatih9@gmail.com>
2026-03-28 16:48:49 +0300

    Proje dosyası ekle.

---
+++ Arac_Kiralama/FrmMusteriGiris.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Arac_Kiralama
+{
+    partial class FrmMusteriGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMusteriGiris));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            maskedTextBox1 = new MaskedTextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 226);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 31);
+            label1.TabIndex = 0;
+            label1.Text = "TC Kimlik No:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(93, 275);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 31);
+            label2.TabIndex = 0;
+            label2.Text = "Şifre:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(151, 275);
+            textBox1.Margin = new Padding(4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(252, 38);
+            textBox1.TabIndex = 1;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(151, 226);
+            maskedTextBox1.Margin = new Padding(4);
+            maskedTextBox1.Mask = "00000000000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(252, 38);
+            maskedTextBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(151, 321);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(168, 40);
+            button1.TabIndex = 3;
+            button1.Text = "Giriş Yap";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(326, 326);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(82, 31);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Üye Ol";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(426, 144);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // FrmMusteriGiris
+            // 
+            AutoScaleDimensions = new SizeF(13F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(449, 449);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            Margin = new Padding(5);
+            Name = "FrmMusteriGiris";
+            Text = "FrmMusteriGiris";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private MaskedTextBox maskedTextBox1;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Arac_Kiralama/FrmMusteriGiris.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Arac_Kiralama
+{
+    public partial class FrmMusteriGiris : Form
+    {
+        public FrmMusteriGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FrmMusteriKayıt fr=new FrmMusteriKayıt();
+            fr.Show();
+            this.Hide();
+        }
+    }
+}
